796a9bbdcffd2b8ff6347ecb3e02767f243489bf
Ousama boujaouane <oboujaouane@gmail.com>
2020-10-19 08:30:06 +0200

    Add UITabBarController and three UIViewControllers

---
+++ LeBaluchon/Controller/ExchangeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExchangeViewController.swift
+//  LeBaluchon
+//
+//  Created by ousama boujaouane on 19/10/2020.
+//  Copyright © 2020 Sajid. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("ExchangeViewController loaded")
+    }
+    
+}
---
+++ LeBaluchon/Controller/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherViewController.swift
+//  LeBaluchon
+//
+//  Created by ousama boujaouane on 19/10/2020.
+//  Copyright © 2020 Sajid. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("WeatherViewController loaded")
+    }
+
+}
