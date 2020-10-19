0581dcc0c59440e64b6faf4bf217702ffa7835d8
Ousama boujaouane <oboujaouane@gmail.com>
2020-10-19 08:10:51 +0200

    Add podfile with SwiftLint + clean title project name

---
+++ LeBaluchon/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  LeBaluchon
+//
+//  Created by ousama boujaouane on 18/10/2020.
+//  Copyright © 2020 Sajid. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    // swiftlint:disable:next line_length
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
