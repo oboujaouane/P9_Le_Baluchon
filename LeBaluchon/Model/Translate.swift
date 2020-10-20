340d6b14e78214a0a9ffed73295c636eed9e39f8
Ousama boujaouane <oboujaouane@gmail.com>
2020-10-20 12:55:17 +0200

    Add APIKeys and struct Translate codable files

---
+++ LeBaluchon/Model/Translate.swift
@@ -0,0 +1,35 @@
+//
+//  Translate.swift
+//  LeBaluchon
+//
+//  Created by ousama boujaouane on 20/10/2020.
+//  Copyright © 2020 Sajid. All rights reserved.
+//
+
+/*
+Translate WS returns:
+ {
+     "data": {
+         "translations": [
+             {
+                 "translatedText": "Hello how are you ?"
+             }
+         ]
+     }
+ }
+*/
+
+/**
+ Reflect the Google Translation API response body JSON structure to decode Google response
+*/
+struct Translate: Codable {
+    let data: Translations
+
+    struct Translations: Codable {
+        var translations: [TranslatedText]
+        // swiftlint:disable nesting
+        struct TranslatedText: Codable {
+            var translatedText: String
+        }
+    }
+}
---
+++ LeBaluchon/Supporting Files/APIKeys.swift
@@ -0,0 +1,26 @@
+//
+//  APIKeys.swift
+//  LeBaluchon
+//
+//  Created by ousama boujaouane on 20/10/2020.
+//  Copyright © 2020 Sajid. All rights reserved.
+//
+
+struct APIKeys {
+    static let GoogleTranslation = "AIzaSyBoSAy-NccJMmydIsl1B02hUxhc9HpNvf8"
+}
+
+/*
+ POST -> https://translation.googleapis.com/language/translate/v2?key=AIzaSyBoSAy-NccJMmydIsl1B02hUxhc9HpNvf8
+
+ Content-Type: application/json
+ Accept-Charset: utf-8
+ 
+ body JSON example :
+ 
+ {
+   "q": ["Bonjour comment allez-vous ?"],
+   "source": "fr",
+   "target": "en"
+ } 
+*/
